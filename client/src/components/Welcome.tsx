6f9a74898a0a9a92ffe66679c14640fb6ed6e74c
Emmanuel Anyigor <emanuelanyigor@gmail.com>
2025-11-20 10:22:24 +0100

    working on loading screen

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -8,7 +8,7 @@ import { useState } from 'react';
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
 const Welcome = () => {
-    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     const connectWallet = () => {
         console.log('connectWallet');
@@ -50,25 +50,30 @@ const Welcome = () => {
                 </div>
 
                 <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
-                    <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+                    <div 
+                        className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism "
+                    >
                         <div className="flex justify-between flex-col w-full h-full">
-                        <div className="flex justify-between items-start">
-                            <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                            <SiEthereum fontSize={21} color="#fff" />
+                            <div className="flex justify-between items-start">
+                                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                                <SiEthereum fontSize={21} color="#fff" />
+                                </div>
+                                <BsInfoCircle fontSize={17} color="#fff" />
                             </div>
-                            <BsInfoCircle fontSize={17} color="#fff" />
-                        </div>
-                        <div>
-                            <p className="text-white font-light text-sm">
-                            {shortenAddress('')}
-                            </p>
-                            <p className="text-white font-semibold text-lg mt-1">
-                            Ethereum
-                            </p>
-                        </div>
+                            <div>
+                                <p className="text-white font-light text-sm">
+                                {shortenAddress('')}
+                                </p>
+                                <p className="text-white font-semibold text-lg mt-1">
+                                Ethereum
+                                </p>
+                            </div>
                         </div>
                     </div>
-                    <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+
+                    <div 
+                        className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism"
+                    >
                         <Input 
                             placeholder="Address To" 
                             name="addressTo" 
@@ -97,13 +102,13 @@ const Welcome = () => {
                         <div className="h-[1px] w-full bg-gray-400 my-2" />
 
                         {isLoading
-                        ? <Loader />
+                            ? <Loader />
                         : (
                             <button
-                            type="button"
-                            className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+                                type="button"
+                                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
                             >
-                            Send now
+                                Send now
                             </button>
                         )}
                     </div>
