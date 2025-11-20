b4d31fa097fbf4732b2d99de2c98673804ffda2c
Emmanuel Anyigor <emanuelanyigor@gmail.com>
2025-11-20 09:02:56 +0100

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Web3 from 'web3'
+
+function App() {
+
+  return (
+    <div className='container mx-auto'>
+      <h1 className="text-3xl font-bold">Welcome to Kyrpt</h1>
+    </div>
+  )
+}
+
+export default App
